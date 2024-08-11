bb52248839316188be790c886da84e45691bcd36
Sahil Agashe <sahilagashe2002@gmail.com>
2024-08-11 10:50:11 +0530

    Initial Commit

---
+++ SAADemo/SAADemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAADemoApp.swift
+//  SAADemo
+//
+//  Created by SAHIL AMRUT AGASHE on 11/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SAADemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
