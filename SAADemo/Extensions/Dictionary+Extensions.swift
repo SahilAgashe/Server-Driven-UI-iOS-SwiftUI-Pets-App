78573e7e38c5a703e84e01af507ff6bf51f3d1dd
Sahil Agashe <sahilagashe2002@gmail.com>
2024-08-13 17:37:13 +0530

    Using JSONEncoder for creating FeaturedImageUIModel object from Dictionary (using Dictionary -> Data -> Required Object)

--- SAADemo/Extensions/Dictionary+Extensions.swift
+++ SAADemo/Extensions/Dictionary+Extensions.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 
-extension Dictionary {
+extension Dictionary where Dictionary.Key : Encodable, Dictionary.Value: Encodable {
     
     func decode<T: Decodable>() -> T? {
-        guard let data = try? JSONSerialization.data(withJSONObject: self, options: .prettyPrinted) else {
-            return nil
-        }
-        
+//        guard let data = try? JSONSerialization.data(withJSONObject: self, options: .prettyPrinted) else {
+//            return nil
+//        }
+        let data = try! JSONEncoder().encode(self)
         return try? JSONDecoder().decode(T.self, from: data)
     }
 }
